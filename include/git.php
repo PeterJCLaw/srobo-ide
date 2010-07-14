279cd64906ed6f52c1cce4fecab8084de75e38f0
Alastair Lynn <arplynn@gmail.com>
2010-07-14 23:02:39 +0100

    Implemented most of the git stubs.
    Signed-off-by: Alastair Lynn <arplynn@gmail.com>

--- include/git.php
+++ include/git.php
@@ -41,10 +41,18 @@ class GitRepository
 
 	public function reset()
 	{
+		$this->gitExecute("reset --hard");
+		$this->gitExecute("clean -f -d");
 	}
 
 	public function commit($message, $name, $email)
 	{
+		$tmp = tempnam("/tmp", "ide-");
+		file_put_contents($tmp, $message);
+		$this->gitExecute("commit -F $tmp", array('GIT_AUTHOR_NAME'    => $name,
+		                                          'GIT_AUTHOR_EMAIL'   => $email,
+		                                          'GIT_COMMITER_NAME'  => $name,
+		                                          'GIT_COMMITER_EMAIL' => $email));
 	}
 
 	public function listFiles($path)
@@ -53,29 +61,60 @@ class GitRepository
 
 	public function createFile($path)
 	{
+		file_put_contents('', $this->path . "/$path");
+		$this->gitExecute("add $path");
 	}
 
 	public function removeFile($path)
 	{
+		$this->gitExecute("rm -f $path");
+	}
+
+	public function moveFile($src, $dst)
+	{
+		$this->copyFile($src, $dst);
+		$this->removeFile($src);
+	}
+
+	public function copyFile($src, $dst)
+	{
+		$this->createFile($dst);
+		$content = $this->getFile($src);
+		$this->putFile($dst, $content);
 	}
 
 	public function getFile($path, $commit = null) // pass $commit to get a particular revision
 	{
+		if ($commit === null)
+		{
+			return file_get_contents($this->path . "/$path");
+		}
+		else
+		{
+			return '';
+		}
 	}
 
 	public function putFile($path, $content)
 	{
+		file_put_contents($this->path . "/$path", $content);
+		$this->gitExecute("add $path");
 	}
 
 	public function diff($commitOld, $commitNew)
 	{
+		return $this->gitExecute("diff -C -M $commitOld..$commitNew");
 	}
 
-	public function getSource($dest, $commit = 'HEAD')
+	public function cloneSource($dest, $commit = 'HEAD')
 	{
+		// TODO: fix to actually obey commit
+		shell_exec("git clone " . $this->path . " $commit");
+		shell_exec("rm -rf $dest/.git");
 	}
 
 	public function revert($commit)
 	{
+		$this->gitExecute("revert $commit");
 	}
 }
