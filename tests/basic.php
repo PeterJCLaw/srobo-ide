985aa9cc034d9a525676e14c2e26ea5142714340
Alastair Lynn <arplynn@gmail.com>
2010-07-15 01:09:52 +0100

    Added testing helper functions.
    Signed-off-by: Alastair Lynn <arplynn@gmail.com>

--- tests/basic.php
+++ tests/basic.php
@@ -1,4 +1,4 @@
 <?php
 
 // this test simply checks for sanity
-require_once('include/main.php');
+// IE: does nothing
--- tests/test-header.php
+++ tests/test-header.php
@@ -1,3 +1,50 @@
 <?php
 
 require_once('include/main.php');
+
+function __test($cond, $message)
+{
+	if ($cond)
+		return;
+	$bt = debug_backtrace(false);
+	array_shift($bt);
+	$frame = array_shift($bt);
+	$line = $frame['line'];
+	echo "Test failed on line $line: $message\n";
+	exit(1);
+}
+
+function test_unreachable($message)
+{
+	__test(false, $message);
+}
+
+function test_true($cond, $message)
+{
+	__test($cond, $message);
+}
+
+function test_false($cond, $message)
+{
+	__test(!$cond, $message);
+}
+
+function test_equal($a, $b, $message)
+{
+	__test($a == $b, $message);
+}
+
+function test_nonequal($a, $b, $message)
+{
+	__test($a != $b, $message);
+}
+
+function test_type($a, $t, $message)
+{
+	__test(gettype($a) == $t, $message);
+}
+
+function test_class($a, $c, $message)
+{
+	__test(get_class($a) == $c, $message);
+}
