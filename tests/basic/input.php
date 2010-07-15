064c46295b6e66ae979b5658f47c10aa4e2c2d03
Alastair Lynn <arplynn@gmail.com>
2010-07-15 01:17:16 +0100

    Added input handling test.
    Signed-off-by: Alastair Lynn <arplynn@gmail.com>

---
+++ tests/basic/input.php
@@ -0,0 +1,20 @@
+<?php
+
+// grab the singleton
+$input = Input::getInstance();
+test_nonnull($input, "failed to get input singleton");
+
+// test the request handling
+$input->setRequest('bees/death');
+test_equal($input->getRequestModule(), 'bees', "wrong module on first RQ test");
+test_equal($input->getRequestCommand(), 'death', "wrong command on first RQ test");
+$input->setRequest('bees/cake/death');
+test_equal($input->getRequestModule(), 'bees/cake', "wrong module on second RQ test");
+test_equal($input->getRequestCommand(), 'death', "wrong command on second RQ test");
+
+// test the data handling
+test_null($input->getInput('dogbees'), "undefined input was not null");
+$input->setInput('dogbees', 'moof');
+test_equal($input->getInput('dogbees'), 'moof', "input was incorrect after addition");
+$input->removeInput('dogbees');
+test_null($input->getInput('dogbees'), "input failed to remove key");
