c850268af04f3b05281df3d58fcdcf811c3a34a3
Alastair Lynn <arplynn@gmail.com>
2010-07-15 23:33:23 +0100

    Added singleton loading of auth modules.
    Signed-off-by: Alastair Lynn <arplynn@gmail.com>

--- include/auth/auth.php
+++ include/auth/auth.php
@@ -2,6 +2,21 @@
 
 abstract class AuthBackend
 {
+	private static $singleton = null;
+
+	public static function getInstance()
+	{
+		if (self::$singleton == null)
+		{
+			$config = Configuration::getInstance();
+			$module = $config->getConfig('auth_module');
+			require_once("include/auth/$module.php");
+			$class = caseTransform($module, CASE_SLASHES, CASE_CAMEL_UCFIRST) . 'Auth';
+			self::$singleton = new $class();
+		}
+		return self::$singleton;
+	}
+
     abstract public function getCurrentUser();
     abstract public function authUser($authToken);
     abstract public function deauthUser($authToken);
