74ec85d2dbe1e06d23d3ab183ec9cfe2f772cf90
Alastair Lynn <arplynn@gmail.com>
2010-07-15 20:46:02 +0100

    Fixed user.php for compliance with style, and sanity.
    Signed-off-by: Alastair Lynn <arplynn@gmail.com>

--- include/user.php
+++ include/user.php
@@ -3,31 +3,35 @@
 class UserInfo
 {
     private static $instance = null;
-
+	private $info = array();
 
     //TODO: things here
-    private function __construct($user_auth_token) {
-        $this->info = array();
+    private function __construct($user_auth_token)
+    {
     }
 
-    public static function getInstance() {
-        if (self::$instance == null) {
+    public static function getInstance()
+    {
+        if (self::$instance == null)
+        {
             $input = Input::getInstance();
             self::$instance = new UserInfo($input->getInput("auth_token"));
         }
 
         return self::$instance;
-
     }
 
 
-    public function getInfo($info) {
-        if (isset($this->info, $info)) {
+    public function getInfo($info)
+    {
+        if (isset($this->info[$info]))
+        {
             return $this->info[$info];
-        } else {
+        }
+        else
+        {
             throw new Exception("key in user info did not exist", 4);
         }
-
     }
 
 }
