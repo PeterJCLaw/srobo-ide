a8d1ac54152436579c78064bb7c566014f703997
Alastair Lynn <arplynn@gmail.com>
2010-07-17 15:35:08 +0100

    Added symbolic names for standard errors.
    Signed-off-by: Alastair Lynn <arplynn@gmail.com>

---
+++ include/errors.php
@@ -0,0 +1,9 @@
+<?php
+
+define('E_MALFORMED_REQUEST', 1);
+define('E_INTERNAL_ERROR',    2);
+define('E_BAD_AUTH_TOKEN',    3);
+define('E_PERM_DENIED',       4);
+define('E_AUTH_FAILED',       5);
+define('E_AUTH_DENIED',       6);
+define('E_NOT_IMPL',          7);
--- include/main.php
+++ include/main.php
@@ -9,3 +9,4 @@ require_once('include/git.php');
 require_once('include/case-transform.php');
 require_once('include/auth/auth.php');
 require_once('include/project-manager.php');
+require_once('include/errors.php');
