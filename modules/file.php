a607d9037e864044cc8791bd75d46d1ad7d7fbf5
Alastair Lynn <arplynn@gmail.com>
2010-07-17 16:05:43 +0100

    Added the file module, with all but 'lint' implemented.
    Signed-off-by: Alastair Lynn <arplynn@gmail.com>

---
+++ modules/file.php
@@ -0,0 +1,115 @@
+<?php
+
+class FileModule extends Module
+{
+	private $team;
+	private $projectName;
+
+	public function __construct()
+	{
+		$auth = AuthBackend::getInstance();
+
+        // bail if we aren't authenticated
+        if ($auth->getCurrentUser() == null)
+        {
+        	// module does nothing if no authentication
+            return;
+        }
+
+		$this->projectManager = ProjectManager::getInstance();
+
+		$input = Input::getInstance();
+		$this->team = $input->getInput("team");
+
+		// check that the project exists and is a git repo otherwise construct
+		// the project directory and git init it
+		$project = $input->getInput("project");
+
+		$this->openProject($this->team, $project);
+		$this->projectName = $project;
+
+		$this->installCommand('list', array($this, 'listFiles'));
+		$this->installCommand('get', array($this, 'getFile'));
+		$this->installCommand('put', array($this, 'putFile'));
+		$this->installCommand('new', array($this, 'newFile'));
+		$this->installCommand('del', array($this, 'deleteFile'));
+		$this->installCommand('cp', array($this, 'copyFile'));
+		$this->installCommand('mv', array($this, 'moveFile'));
+	}
+
+	private function verifyTeam()
+	{
+		$auth = AuthBackend::getInstance();
+		if (!in_array($this->team, $auth->getCurrentUserGroups()))
+		{
+			throw new Exception("proj attempted on team you aren't in", E_PERM_DENIED);
+		}
+	}
+
+	private function repository()
+	{
+		$pm = ProjectManager::getInstance();
+		$this->verifyTeam();
+		$repo = $pm->getRepository($this->team, $this->projectName);
+		return $repo;
+	}
+
+	public function listFiles()
+	{
+		$input  = Input::getInstance();
+		$output = Output::getInstance();
+		$path   = $input->getInput('path');
+		$output->setOutput('files', $this->repository()->listFiles($path));
+	}
+
+	public function getFile()
+	{
+		$input  = Input::getInstance();
+		$output = Output::getInstance();
+		$path   = $input->getInput('path');
+		$output->setOutput('data', $this->repository()->getFile($path));
+	}
+
+	public function putFile()
+	{
+		$input  = Input::getInstance();
+		$output = Output::getInstance();
+		$path   = $input->getInput('path');
+		$data   = $input->getInput('data');
+		$this->repository()->putFile($path, $data);
+	}
+
+	public function newFile()
+	{
+		$input  = Input::getInstance();
+		$output = Output::getInstance();
+		$path   = $input->getInput('path');
+		$this->repository()->createFile($path);
+	}
+
+	public function deleteFile()
+	{
+		$input  = Input::getInstance();
+		$output = Output::getInstance();
+		$path   = $input->getInput('path');
+		$this->repository()->removeFile($path);
+	}
+
+	public function copyFile()
+	{
+		$input   = Input::getInstance();
+		$output  = Output::getInstance();
+		$oldPath = $input->getInput('old-path');
+		$newPath = $input->getInput('new-path');
+		$this->repository()->copyFile($oldPath, $newPath);
+	}
+
+	public function moveFile()
+	{
+		$input   = Input::getInstance();
+		$output  = Output::getInstance();
+		$oldPath = $input->getInput('old-path');
+		$newPath = $input->getInput('new-path');
+		$this->repository()->moveFile($oldPath, $newPath);
+	}
+}
