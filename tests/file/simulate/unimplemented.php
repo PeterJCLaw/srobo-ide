ac445bbb14891020ffadf2d735db0a43c996fc28
Alastair Lynn <arplynn@gmail.com>
2010-07-17 12:29:00 +0100

    Removed some unnecessary testing from tests/file/simulate.
    Signed-off-by: Alastair Lynn <arplynn@gmail.com>

--- tests/file/simulate/unimplemented.php
+++ tests/file/simulate/unimplemented.php
@@ -16,30 +16,3 @@ test_exception(function () use ($mod) { $mod->dispatchCommand('end'); },
                7, "'end' did not throw");
 test_exception(function () use ($mod) { $mod->dispatchCommand('status'); },
                7, "'status' did not throw");
-
-try
-{
-	$mod->dispatchCommand('begin');
-	test_unreachable("'begin' did not throw");
-}
-catch (Exception $e)
-{
-}
-
-try
-{
-	$mod->dispatchCommand('end');
-	test_unreachable("'end' did not throw");
-}
-catch (Exception $e)
-{
-}
-
-try
-{
-	$mod->dispatchCommand('status');
-	test_unreachable("'status' did not throw");
-}
-catch (Exception $e)
-{
-}
