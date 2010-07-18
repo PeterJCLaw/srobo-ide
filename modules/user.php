87a9b0bd093d58363e38742419e1f6a68e21e713
Alastair Lynn <arplynn@gmail.com>
2010-07-18 11:43:02 +0100

    Added the user module.
    Signed-off-by: Alastair Lynn <arplynn@gmail.com>

---
+++ modules/user.php
@@ -0,0 +1,23 @@
+<?php
+
+class UserModule extends Module
+{
+	public function __construct()
+	{
+		$this->installCommand('info', array($this, 'getInfo'));
+	}
+
+	public function getInfo()
+	{
+		$output = Output::getInstance();
+		$auth   = AuthBackend::getInstance();
+		if (!($username = $auth->getCurrentUser()))
+		{
+			throw new Exception('you are not logged in', E_PERM_DENIED);
+		}
+		$output->setOutput('display-name', $auth->displayNameForUser($username));
+		$output->setOutput('email', $auth->emailForUser($username));
+		$output->setOutput('teams', $auth->getCurrentUserGroups());
+		$output->setOutput('is-admin', false);
+	}
+}
