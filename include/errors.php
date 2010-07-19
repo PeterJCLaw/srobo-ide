c6d4d32fb7fd8e2b1d1318ac0263df7f4124502e
Alastair Lynn <arplynn@gmail.com>
2010-07-19 06:54:52 +0100

    Updated the error list.
    Signed-off-by: Alastair Lynn <arplynn@gmail.com>

--- include/errors.php
+++ include/errors.php
@@ -7,5 +7,5 @@ define('E_PERM_DENIED',       4);
 define('E_AUTH_FAILED',       5);
 define('E_AUTH_DENIED',       6);
 define('E_NOT_IMPL',          7);
-define('E_PROJ_NONEXISTANT', 0x21);
-define('E_LDAP_NOT_AUTHED', 0x41);
+define('E_PROJ_NONEXISTANT',  8);
+define('E_LDAP_NOT_AUTHED',   9);
