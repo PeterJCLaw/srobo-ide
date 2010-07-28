010cb4a17bac43720904ea448f4e896875d7d946
Alastair Lynn <arplynn@gmail.com>
2010-07-28 18:42:28 +0100

    Added tests.
    Signed-off-by: Alastair Lynn <arplynn@gmail.com>

---
+++ tests/ping/ping.php
@@ -0,0 +1,19 @@
+<?php
+
+$config = Configuration::getInstance();
+$config->override('modules', array('ping'));
+
+$mm = ModuleManager::getInstance();
+$mm->importModules();
+test_true($mm->moduleExists('ping'), "ping module was not found");
+
+$module = $mm->getModule('ping');
+
+$input = Input::getInstance();
+$output = Output::getInstance();
+
+$module->dispatchCommand('ping');
+
+$input->setInput('data', 'bees');
+$module->dispatchCommand('ping');
+test_equal($output->getOutput('data'), 'bees', 'ping response was incorrect');
