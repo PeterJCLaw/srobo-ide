51d0e3e36c92dc4bacd77d1ce1a8cf2c584a9b3d
Sam Phippen <samphippen@googlemail.com>
2010-08-31 23:53:37 +0100

    add some docstrings to token strategy

--- include/auth/tokenstrategy/tokenstrategy.php
+++ include/auth/tokenstrategy/tokenstrategy.php
@@ -1,11 +1,30 @@
 <?php
 
+/**
+ * Base class for token strategies
+ *
+ * There was some lengthy discussion about the best way to handle tokens, this
+ * can be used to easily manufacture and swap token strategies based on what is
+ * perceived to be best at the time
+ *
+ * @author Sam Phippen <samphippen@googlemail.com>
+ */
 abstract class TokenStrategy
 {
+    /**
+     * Returns the current auth token supplied on the request
+     */
     abstract public function getAuthToken();
+
+    /**
+     * Sets the auth token that should be passed on the next request
+     */
     abstract public function setNextAuthToken($token);
 }
 
+/**
+ * returns an object for the default strategy for handling auth tokens
+ */
 function getDefaultTokenStrategy()
 {
     $name = Configuration::getInstance()->getConfig("default_token_strategy");
