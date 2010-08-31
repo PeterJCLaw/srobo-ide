73e300537c90b1aee5ddfbe2e750e1f9a34b2733
Sam Phippen <samphippen@googlemail.com>
2010-08-31 23:42:42 +0100

    make the default token strategy a config option
    better because people don't have to go into code to change this, also modifies
    errors because there is an error if you pass an invalid class into the config
    string

--- include/auth/tokenstrategy/tokenstrategy.php
+++ include/auth/tokenstrategy/tokenstrategy.php
@@ -8,5 +8,10 @@ abstract class TokenStrategy
 
 function getDefaultTokenStrategy()
 {
-    return new IOTokenStrategy();
+    $name = Configuration::getInstance()->getConfig("default_token_strategy");
+    if (!class_exists($name) || !is_subclass_of($this, $name))
+    {
+        throw new Exception("the default token strategy is not valid", E_TOKEN_STRAT_CONFIG);
+    }
+    return new $name();
 }
--- include/errors.php
+++ include/errors.php
@@ -1,12 +1,13 @@
 <?php
 
-define('E_MALFORMED_REQUEST', 1);
-define('E_INTERNAL_ERROR',    2);
-define('E_BAD_AUTH_TOKEN',    3);
-define('E_PERM_DENIED',       4);
-define('E_AUTH_FAILED',       5);
-define('E_AUTH_DENIED',       6);
-define('E_NOT_IMPL',          7);
-define('E_PROJ_NONEXISTANT',  8);
-define('E_LDAP_NOT_AUTHED',   9);
-define('E_NO_EXPIRY_TIME',   10);
+define('E_MALFORMED_REQUEST',   1);
+define('E_INTERNAL_ERROR',      2);
+define('E_BAD_AUTH_TOKEN',      3);
+define('E_PERM_DENIED',         4);
+define('E_AUTH_FAILED',         5);
+define('E_AUTH_DENIED',         6);
+define('E_NOT_IMPL',            7);
+define('E_PROJ_NONEXISTANT',    8);
+define('E_LDAP_NOT_AUTHED',     9);
+define('E_NO_EXPIRY_TIME',     10);
+define('E_TOKEN_STRAT_CONFIG', 11);
