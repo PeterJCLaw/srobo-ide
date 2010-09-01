e641a0d1eb6f92071e1f17effaa10875ad6299f2
Sam Phippen <samphippen@googlemail.com>
2010-09-01 02:17:51 +0100

    fix class sanity check on default token factory

--- include/auth/tokenstrategy/tokenstrategy.php
+++ include/auth/tokenstrategy/tokenstrategy.php
@@ -33,7 +33,7 @@ abstract class TokenStrategy
 function getDefaultTokenStrategy()
 {
     $name = Configuration::getInstance()->getConfig("default_token_strategy");
-    if (!class_exists($name) || !is_subclass_of($this, $name))
+    if (!class_exists($name) || !is_subclass_of(new $name(), 'TokenStrategy'))
     {
         throw new Exception("the default token strategy is not valid", E_TOKEN_STRAT_CONFIG);
     }
