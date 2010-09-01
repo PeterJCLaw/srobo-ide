c9fdcc95d98138a9a89145e0c82cf158e18dd546
Peter Law <PeterJCLaw@gmail.com>
2010-09-01 23:02:57 +0100

    Whitespace fix in cookiestrategy

--- include/auth/tokenstrategy/cookiestrategy.php
+++ include/auth/tokenstrategy/cookiestrategy.php
@@ -4,34 +4,34 @@ require_once("tokenstrategy.php");
 
 class CookieStrategy extends TokenStrategy
 {
-    /**
-     * the name for the cookie
-     */
-    const COOKIENAME = "token";
+	/**
+	 * the name for the cookie
+	 */
+	const COOKIENAME = "token";
 
-    public function getAuthToken()
-    {
-        return $_COOKIE[self::COOKIENAME];
-    }
+	public function getAuthToken()
+	{
+		return $_COOKIE[self::COOKIENAME];
+	}
 
-    public function setNextAuthToken($token)
-    {
-        $expiry = Configuration::getInstance()->getConfig("cookie_expiry_time");
-        if ($expiry == null)
-        {
-            throw new Exception("no cookie expiry time found in config file", E_NO_EXPIRY_TIME);
-        }
-        setcookie(
-                  self::COOKIENAME,
-                  $token,
-                  time() + (int)$expiry,
-                  $_SERVER['SCRIPT_NAME']
-                 );
-    }
+	public function setNextAuthToken($token)
+	{
+		$expiry = Configuration::getInstance()->getConfig("cookie_expiry_time");
+		if ($expiry == null)
+		{
+			throw new Exception("no cookie expiry time found in config file", E_NO_EXPIRY_TIME);
+		}
+		setcookie(
+		          self::COOKIENAME,
+		          $token,
+		          time() + (int)$expiry,
+		          $_SERVER['SCRIPT_NAME']
+		         );
+	}
 
-    public function removeAuthToken()
-    {
-        setcookie (self::COOKIENAME, "", time() - 3600);
-    }
+	public function removeAuthToken()
+	{
+		setcookie(self::COOKIENAME, "", time() - 3600);
+	}
 
 }
