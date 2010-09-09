e78c8935a32fe25c103d5303e6320b638416b590
Alastair Lynn <arplynn@gmail.com>
2010-09-09 20:31:23 +0100

    Added some file utils.
    Signed-off-by: Alastair Lynn <arplynn@gmail.com>

---
+++ include/file-utils.php
@@ -0,0 +1,40 @@
+<?php
+
+function path_walk($callback, $path, $reverse = false)
+{
+	$parts = explode('/', $path);
+	if ($parts[0] == '')
+	{
+		$start = 1;
+		$prefix = '';
+	}
+	else
+	{
+		$start = 0;
+		$prefix = '.';
+	}
+	$paths = array();
+	for ($i = $start; $i <= count($parts); ++$i)
+	{
+		$paths[] = $prefix . '/' . implode('/', array_slice($parts, $start, $i - $start));
+	}
+	if ($reverse)
+		$paths = array_reverse($paths);
+	foreach ($paths as $path)
+		$callback($path);
+}
+
+function delete_recursive($path)
+{
+	system("rm -rf $path");
+}
+
+function mkdir_full($path, $mode = 0755)
+{
+	path_walk(function ($path) use ($mode) {
+		if (!file_exists($path))
+		{
+			mkdir($path, $mode);
+		}
+	}, $path, false);
+}
