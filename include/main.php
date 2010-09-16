1bb77c1d118c779d1b8f6be7796d545fcf1f65a3
Peter Law <PeterJCLaw@gmail.com>
2010-09-16 21:59:49 +0100

    Sort PHP includes

--- include/main.php
+++ include/main.php
@@ -1,14 +1,14 @@
 <?php
 
-require_once('include/config.php');
-require_once('include/input.php');
-require_once('include/user.php');
-require_once('include/output.php');
-require_once('include/module.php');
-require_once('include/git.php');
-require_once('include/case-transform.php');
 require_once('include/auth/auth.php');
-require_once('include/project-manager.php');
-require_once('include/errors.php');
 require_once('include/auth/tokenstrategy/cookiestrategy.php');
 require_once('include/auth/tokenstrategy/tokenstrategy.php');
+require_once('include/case-transform.php');
+require_once('include/config.php');
+require_once('include/errors.php');
+require_once('include/git.php');
+require_once('include/input.php');
+require_once('include/module.php');
+require_once('include/output.php');
+require_once('include/project-manager.php');
+require_once('include/user.php');
