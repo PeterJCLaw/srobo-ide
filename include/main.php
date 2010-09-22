700f63ab7346e97333e9b5446a673c252e6ae6f5
Sam Phippen <samphippen@googlemail.com>
2010-09-22 19:29:05 +0100

    input needs to be above ide log
    Signed-off-by: Sam Phippen <samphippen@googlemail.com>

--- include/main.php
+++ include/main.php
@@ -3,6 +3,8 @@
 if (!defined('IN_TESTS'))
 	define('IN_TESTS', 0);
 
+require_once('include/input.php');
+
 function ide_log($message)
 {
 	if (IN_TESTS)
@@ -39,7 +41,6 @@ require_once('include/case-transform.php');
 require_once('include/config.php');
 require_once('include/errors.php');
 require_once('include/git.php');
-require_once('include/input.php');
 require_once('include/module.php');
 require_once('include/output.php');
 require_once('include/project-manager.php');
