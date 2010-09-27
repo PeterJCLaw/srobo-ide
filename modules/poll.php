f6aed876a97da5d996fae6f294be12b2b350920d
Peter Law <PeterJCLaw@gmail.com>
2010-09-27 20:30:00 +0100

    Add the backend poll module.
    Currently it's just a shell, but will eventually provide the
    frontend poll module with all sorts of useful information.

---
+++ modules/poll.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * Module for polling data from the server
+ */
+class PollModule extends Module
+{
+	private $username;
+
+	public function __construct()
+	{
+		$this->installCommand('poll', array($this, 'poll'));
+	}
+
+	/**
+	 * Ensures that we have a valid user.
+	 * You can't do anything user related without being authed, but putting
+	 * this in the constructor causes issues, since construction occurs
+	 * before the auth cycle does.
+	 * Returns the AuthBackend instance for convenience.
+	 */
+	private function ensureAuthed()
+	{
+		$auth = AuthBackend::getInstance();
+		if (!($this->username = $auth->getCurrentUser()))
+		{
+			throw new Exception('you are not logged in', E_PERM_DENIED);
+		}
+		return $auth;
+	}
+
+	/**
+	 * Return a collection of useful pieces of information about the current
+	 * state of the repos, teams, etc. that this user is part of.
+	 */
+	public function poll()
+	{
+		$this->ensureAuthed();
+	}
+}
