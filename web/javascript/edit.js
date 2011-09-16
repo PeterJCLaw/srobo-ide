c83072af794ff17e23c45fb189dd71f70e1f246d
Peter Law <PeterJCLaw@gmail.com>
2011-09-16 18:34:09 +0100

    Move the common things outside the if..else.

--- web/javascript/edit.js
+++ web/javascript/edit.js
@@ -388,18 +388,14 @@ function EditTab(iea, team, project, path, rev, mode) {
 			this._original = this.contents;
 			this.tab.set_label(this.path);
 			this._autosaved = "";
-			this._isNew = false;
-			this.rev = nodes.commit;
-			$("check-syntax").disabled = false;
-			this._update_contents();
 		} else {
 			status_msg("File "+this.path+" Merge required, please check and try again (Now at "+nodes.commit+")", LEVEL_ERROR);
 			this.contents = nodes.code;
-			this._isNew = false;
-			this.rev = nodes.commit;
-			$("check-syntax").disabled = false;
-			this._update_contents();
 		}
+		this._isNew = false;
+		this.rev = nodes.commit;
+		$("check-syntax").disabled = false;
+		this._update_contents();
 	}
 
 	//ajax event handler for saving to server
