727d48220f3b5a36c2e1a86ae369b9c951fb4e7c
Alastair Lynn <arplynn@gmail.com>
2011-09-16 23:11:49 +0100

    Added a new 'auth required' error code
    This will be used rather than 'permission denied' in cases where logging in
    is the only barrier to this endpoint's validity.
    
    Signed-off-by: Alastair Lynn <arplynn@gmail.com>

--- include/errors.php
+++ include/errors.php
@@ -11,3 +11,4 @@ define('E_PROJ_NONEXISTANT',    8);
 define('E_LDAP_NOT_AUTHED',     9);
 define('E_NO_EXPIRY_TIME',     10);
 define('E_TOKEN_STRAT_CONFIG', 11);
+define('E_AUTH_REQUIRED',      12);
