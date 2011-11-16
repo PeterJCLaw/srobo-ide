b5cb389723fdafbec0748275c0b8fe0604999281
Peter Law <PeterJCLaw@gmail.com>
2011-11-16 22:51:56 +0000

    Add tests for exporting various project names.
    There's a bug when you export project names with '?' in them, which
    is a valid name, but of course messes with the URLs.

---
+++ tests/proj/export2.php
@@ -0,0 +1,86 @@
+<?php
+
+$input = Input::getInstance();
+$config = Configuration::getInstance();
+
+function cleanCreate($path) {
+	delete_recursive($path);
+	mkdir_full($path);
+}
+
+cleanCreate('/tmp/proj-export/wd');
+cleanCreate('/tmp/proj-export/test-repos');
+$test_zip_path = '/tmp/proj-export/ide-test-zip';
+cleanCreate($test_zip_path);
+
+// remove the folder so that we can test the failure mode
+$zipPathBase = '/tmp/proj-export/ide-zips';
+delete_recursive($zipPathBase);
+
+$config->override('repopath', '/tmp/proj-export/test-repos');
+
+$config->override('zippath', $zipPathBase);
+$config->override('zipurl', $zipPathBase);
+$config->override('user.default', 'death');
+$config->override('user.default.teams', array(1, 2));
+$config->override('auth_module', 'single');
+$config->override('modules', array('proj', 'file'));
+
+$auth = AuthBackend::getInstance();
+test_true($auth->authUser('death','face'), 'authentication failed');
+
+$input->setInput('team', 1);
+$input->setInput('rev', 'HEAD');
+
+$mm = ModuleManager::getInstance();
+$mm->importModules();
+test_true($mm->moduleExists('proj'), 'proj module does not exist');
+$proj = $mm->getModule('proj');
+
+function createAndExportProject($name)
+{
+	section("Test exporting a project called '$name'");
+	$output = Output::getInstance();
+	$input = Input::getInstance();
+	$input->setInput('project', $name);
+
+	// need to do this each time since the modules are single-shot
+	// that is, they only get one Input per instance, and may cache what it has to say.
+	$mm = ModuleManager::getInstance();
+	$mm->importModules();
+
+	subsection('create');
+	$proj = $mm->getModule('proj');
+	test_true($proj->dispatchCommand('new'), "Failed to create project '$name'");
+
+	subsection('export');
+	test_true($proj->dispatchCommand('co'), "Failed to export project '$name'");
+	$zip_path = $output->getOutput('url');
+	var_dump($zip_path);
+	test_false(strpos($zip_path, '?'), 'Zip path must not contain \'?\' to be exported successfully');
+	$parsed = parse_url($zip_path);
+	$path = urldecode($parsed['path']);
+	test_true(file_exists($path), "Zip path must survive being parsed by a webserver, and then exist. Original: '$zip_path', Parsed: '$path'.");
+	// TODO: actually try http-GET-ing the file?
+}
+
+// samples based on issue #1002
+$names = array('beans?', 'bacon?spam&jam', 'spam&jam');
+foreach ($names as $name)
+{
+	createAndExportProject($name);
+}
+
+// copied from proj/create. TODO: make this a common list
+$chars = '$%@~{}][()';
+for($i=0; $i < strlen($chars); $i++)
+{
+	createAndExportProject('char \''.$chars[$i].'\'.');
+}
+
+// copied from proj/create. TODO: make this a common list
+$unicodes = array('£', '❝', '♞');
+foreach($unicodes as $char)
+{
+	createAndExportProject('char \''.$char.'\'.');
+}
