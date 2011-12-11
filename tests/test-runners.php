4a6614d2b5adf094c4c733be1b35bdd50ef515f4
Peter Law <PeterJCLaw@gmail.com>
2011-12-11 16:19:29 +0000

    Make it possible to run tests written in other languages.
    Currently we only support php, but I'd like to be able to write tests
    in other languages. In particular, I'm planning to add some http
    and multi-threaded tests in python.

---
+++ tests/test-runners.php
@@ -0,0 +1,71 @@
+<?php
+
+interface ITestRunner
+{
+	function run($inFile);
+}
+
+abstract class BaseRunner implements ITestRunner
+{
+	protected $name = null;
+
+	/**
+	 * Runs a given command.
+	 * Note: this method is a dumb (ish) wrapper around proc_open,
+	 *  so you need to do your own shell escaping.
+	 * @returns: An array containing:
+	 *    0: the return code
+	 *    1: the combined StdOut and StdErr
+	 */
+	protected function runCommand($s_command, $s_inFile)
+	{
+		$process = proc_open("$s_command 2>&1", array(0 => array('file', $s_inFile, 'r'),
+	                                                  1 => array('pipe', 'w'),
+	                                                  2 => array('file', '/dev/null', 'w')),
+	                                            $pipes);
+		$output = trim(stream_get_contents($pipes[1]));
+		$rc = proc_close($process);
+		return array($rc, $output);
+	}
+
+	public function __construct($name)
+	{
+		$this->name = $name;
+	}
+
+	static $runners = array('php' => 'PHPRunner', 'py' => 'PythonRunner');
+
+	private static function realpath($name)
+	{
+		return realpath('tests/'.$name);
+	}
+
+	public static function createRunner($name)
+	{
+		foreach (self::$runners as $ext => $runnerClass)
+		{
+			if ($realpath = self::realpath($name.'.'.$ext))
+			{
+				return new $runnerClass($realpath);
+			}
+		}
+		return new FailRunner($name);
+	}
+}
+
+class PHPRunner extends BaseRunner
+{
+	public function run($inFile)
+	{
+		$helper = realpath('tests/test-helper.php');
+		return parent::runCommand("php $helper $this->name", $inFile);
+	}
+}
+
+class FailRunner extends BaseRunner
+{
+	public function run($inFile)
+	{
+		return array(-1, "No suitable runner found for $this->name");
+	}
+}
