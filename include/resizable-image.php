b952e19fdcd596aafff243561fd77e371ca21771
Richard Barlow <richard@richardbarlow.co.uk>
2012-01-24 04:29:35 +0000

    Support transparency when resizing images

--- include/resizable-image.php
+++ include/resizable-image.php
@@ -32,6 +32,8 @@ class ResizableImage
 		}
 
 		$newImage = imagecreatetruecolor($width, $height);
+		imagealphablending($newImage, false);
+		imagesavealpha($newImage, true);
 		$curImage = $this->loadFromFile();
 
 		// can go no further if we don't have the resources.
