srobo-legacy/cyanide

Avoid locks in the master repo just to clone it.
Expose the checks done to 'ensure' that a given path is a valid
bare repository. This allows this check to be done without needing
to instantiate a GitRepository, removing the need for a lock on it.

This should make per-user clones more efficient, while containing
the same level of safety checks.

Change-Id: I138a2f5ce1ef193b41ccc54b762b2a7760ad2d27

include/project-manager.php
@@ -100,8 +100,9 @@ class ProjectManager
 		// the user needs a clone of an existing master
 		else
 		{
-			$master = $this->getMasterRepository($team, $project);
-			$masterPath = $master->gitPath();
+			$masterPath = $this->getMasterRepositoryPath($team, $project);
+			// check this is valid (throws if not) - master repos are bare.
+			GitRepository::EnsureBareRepo($masterPath);
 			return GitRepository::cloneRepository($masterPath, $path);
 		}
 	}

PeterJCLaw/srobo-ide

include/project-manager.php
@@ -101,8 +101,9 @@ class ProjectManager
 		// the user needs a clone of an existing master
 		else
 		{
-			$master = $this->getMasterRepository($team, $project);
-			$masterPath = $master->gitPath();
+			$masterPath = $this->getMasterRepoPath($team, $project);
+			// check this is valid (throws if not) - master repos are bare.
+			GitRepository::EnsureBareRepo($masterPath);
 			return GitRepository::cloneRepository($masterPath, $path);
 		}
 	}