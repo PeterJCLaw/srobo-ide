srobo-legacy/cyanide

Separate the UI from its need to have a team id.
There is now quite a lot of functionality in the IDE that doesn't
actually need a team id to work, and it's quite possible that
mentors won't be in a specific team. This commit rearranges how
the UI is launched so that it is less dependant on the user being
in a team. Add a useful message for users that aren't in teams,
and add checks where needed that the user has selected a team,
with an instructional error if they haven't.

This commit also includes quite a bit of restructuring to the
ProjPage object which simplifies the UI setup by moving stuff that
should be internal to the ProjPage into it. Some of these things
are then re-exposed if needed, but the majority aren't.

Change-Id: I18222e407312f024e035e053d5f3cd8013933a4b

web/javascript/errors.js
@@ -145,7 +145,7 @@ function ErrorsPage() {
 				opts.callback('pass');
 			}
 			//if not (quiet if pass or a mulifile call from the projpage and no errors yet and this is not the last one to check)
-			if( !( opts != null && (opts.quietpass || opts.projpage_multifile && projtab.has_focus() && async_count > 1) ) )
+			if( !( opts != null && (opts.quietpass || opts.projpage_multifile && projpage.has_focus() && async_count > 1) ) )
 				this._prompt = status_msg( "No errors found", LEVEL_OK );
 			this._clear_file(file);
 		}

web/javascript/gui.js
@@ -1,5 +1,6 @@
 // Initialise to an invalid team number
-team = 0; /*The current team number*/
+
+team = null; /* The current team id */
 
 MAX_TAB_NAME_LENGTH = 8;
 
@@ -11,9 +12,6 @@ var projpage = null;
 
 // The simulator page
 var simpage = null;
-
-// The project tab
-var projtab = null;
 
 // The edit page
 var editpage = null;
@@ -45,10 +43,6 @@ var diffpage = null;
 // The about box
 var about = null;
 
-// The initial onchange event ident connected to the tabbar
-// Gets disconnected as soon as a team is selected
-var tabchange_ident = null;
-
 function logErrorNice(err) {
 	if (typeof(err) != 'string') {
 		var locLen = window.location.href.length;
@@ -70,33 +64,34 @@ addLoadEvent( function() {
 	user = new User();
 	var d = user.load();
 	// Wait for the user information to come back
-	d.addCallback( load_select_team );
+	d.addCallback( load_gui );
 	d.addErrback( function(err) {
 		logErrorNice(err);
 		window.alert("Failed to get user info: " + err);
 	} );
+	load_gui_initial();
 });
 
-// 1) executed after the user's information has been acquired
-function load_select_team() {
-	// Got the user information -- now get team information
-	team_selector = new TeamSelector();
-
-	projpage = new ProjPage();
-	connect( team_selector, "onchange", bind(projpage.set_team, projpage) );
-	tabchange_ident = connect( team_selector, "onchange", load_gui );
-
-	// Triggers the signals from the team selector
-	team_selector.load();
+function validate_team() {
+	if ( team == null || team == 0 ) {
+		this._prompt = status_msg("You must select a team", LEVEL_WARN);
+		return false;
+	}
+	return true;
 }
 
-// 2) Executed once we have team
-function load_gui() {
-	logDebug( "load_gui" );
-	// We don't want this function to be called again
-	disconnect( tabchange_ident );
+// 1) Load the initial UI elements - those that don't need a team to get going
+function load_gui_initial() {
+	logDebug( "load_gui_initial" );
 
+	// Team selector
+	team_selector = new TeamSelector();
+
+	// Main tab well
 	tabbar = new TabBar();
+
+	// Projects page
+	projpage = new ProjPage(team_selector);
 
 	// Edit page
 	editpage = new EditPage();
@@ -112,6 +107,11 @@ function load_gui() {
 
 	//The Admin page - this must happen before populate_shortcuts_box is called
 	adminpage = new Admin();
+}
+
+// 2) Executed once we have user details
+function load_gui() {
+	logDebug( "load_gui" );
 
 	var shortcutsList = populate_shortcuts_box();
 
@@ -124,11 +124,11 @@ function load_gui() {
 	addElementClass(sbutton._a ,"shortcutButton"); /* add its own class so it can be styled individually */
 	tabbar.add_tab( sbutton );
 
-	// Projects tab
-	projtab = new Tab( "Projects", {can_close:false} );
-	connect( projtab, "onfocus", bind( projpage.show, projpage ) );
-	connect( projtab, "onblur", bind( projpage.hide, projpage ) );
-	tabbar.add_tab( projtab );
+	// Now that the UI is ready, and we have all the info needed so far,
+	// show the user the team selector.
+	// Since this could load the project page (via a default team), it
+	// needs to be after we add the shortcuts box to the tabbar.
+	team_selector.load();
 
 	// Simulator tab
 	//simpage = new SimPage();
@@ -143,11 +143,6 @@ function load_gui() {
 	errorspage = new ErrorsPage();
 
 	robolog = new RoboLog();
-
-	//The selection operations
-	sel_operations = new ProjOps();
-
-	tabbar.switch_to( projtab );
 }
 
 function on_doc_keydown(ev) {
@@ -440,6 +435,11 @@ function TeamSelector() {
 
 		if( user.teams.length == 1 )
 			team = user.teams[0];
+		else if ( user.teams.length == 0 )
+		{
+			replaceChildNodes( "teaminfo", SPAN("You are not in any teams!") );
+			return;
+		}
 		else
 		{
 			var olist = [];

PeterJCLaw/srobo-ide

Change-Id: Ieb3662f449c8f546d1e14fade120b916cdc8f99f

web/javascript/errors.js
@@ -146,7 +146,7 @@ function ErrorsPage() {
 				opts.callback('pass');
 			}
 			//if not (quiet if pass or a mulifile call from the projpage and no errors yet and this is not the last one to check)
-			if( !( opts != null && (opts.quietpass || opts.projpage_multifile && projtab.has_focus() && IDE_async_count > 1) ) )
+			if( !( opts != null && (opts.quietpass || opts.projpage_multifile && projpage.has_focus() && IDE_async_count > 1) ) )
 				this._prompt = status_msg( "No errors found", LEVEL_OK );
 			this._clear_file(file);
 		}

web/javascript/gui.js
@@ -1,5 +1,6 @@
 // Initialise to an invalid team number
-team = 0; /*The current team number*/
+
+team = null; /* The current team id */
 
 MAX_TAB_NAME_LENGTH = 8;
 
@@ -8,9 +9,6 @@ var tabbar = null;
 
 // The project page
 var projpage = null;
-
-// The project tab
-var projtab = null;
 
 // The edit page
 var editpage = null;
@@ -42,10 +40,6 @@ var diffpage = null;
 // The about box
 var about = null;
 
-// The initial onchange event ident connected to the tabbar
-// Gets disconnected as soon as a team is selected
-var tabchange_ident = null;
-
 function logErrorNice(err) {
 	if (typeof(err) != 'string') {
 		var locLen = window.location.href.length;
@@ -67,33 +61,34 @@ addLoadEvent( function() {
 	user = new User();
 	var d = user.load();
 	// Wait for the user information to come back
-	d.addCallback( load_select_team );
+	d.addCallback( load_gui );
 	d.addErrback( function(err) {
 		logErrorNice(err);
 		window.alert("Failed to get user info: " + err);
 	} );
+	load_gui_initial();
 });
 
-// 1) executed after the user's information has been acquired
-function load_select_team() {
-	// Got the user information -- now get team information
-	team_selector = new TeamSelector();
-
-	projpage = new ProjPage();
-	connect( team_selector, "onchange", bind(projpage.set_team, projpage) );
-	tabchange_ident = connect( team_selector, "onchange", load_gui );
-
-	// Triggers the signals from the team selector
-	team_selector.load();
+function validate_team() {
+	if ( team == null || team == 0 ) {
+		this._prompt = status_msg("You must select a team", LEVEL_ERROR);
+		return false;
+	}
+	return true;
 }
 
-// 2) Executed once we have team
-function load_gui() {
-	logDebug( "load_gui" );
-	// We don't want this function to be called again
-	disconnect( tabchange_ident );
+// 1) Load the initial UI elements - those that don't need a team to get going
+function load_gui_initial() {
+	logDebug( "load_gui_initial" );
 
+	// Team selector
+	team_selector = new TeamSelector();
+
+	// Main tab well
 	tabbar = new TabBar();
+
+	// Projects page
+	projpage = new ProjPage(team_selector);
 
 	// Edit page
 	editpage = new EditPage();
@@ -109,6 +104,11 @@ function load_gui() {
 
 	//The Admin page - this must happen before populate_shortcuts_box is called
 	adminpage = new Admin();
+}
+
+// 2) Executed once we have user details
+function load_gui() {
+	logDebug( "load_gui" );
 
 	//The Search page - this must happen before populate_shortcuts_box is called
 	searchpage = SearchPage.GetInstance();
@@ -124,22 +124,17 @@ function load_gui() {
 	addElementClass(sbutton._a ,"shortcutButton"); /* add its own class so it can be styled individually */
 	tabbar.add_tab( sbutton );
 
-	// Projects tab
-	projtab = new Tab( "Projects", {can_close:false} );
-	connect( projtab, "onfocus", bind( projpage.show, projpage ) );
-	connect( projtab, "onblur", bind( projpage.hide, projpage ) );
-	tabbar.add_tab( projtab );
+	// Now that the UI is ready, and we have all the info needed so far,
+	// show the user the team selector.
+	// Since this could load the project page (via a default team), it
+	// needs to be after we add the shortcuts box to the tabbar.
+	team_selector.load();
 
 	// Diff Page
 	diffpage = new DiffPage();
 
 	// Errors Tab
 	errorspage = new ErrorsPage();
-
-	//The selection operations
-	sel_operations = new ProjOps();
-
-	tabbar.switch_to( projtab );
 }
 
 function on_doc_keydown(ev) {
@@ -436,6 +431,11 @@ function TeamSelector() {
 
 		if( user.teams.length == 1 )
 			team = user.teams[0];
+		else if ( user.teams.length == 0 )
+		{
+			replaceChildNodes( "teaminfo", SPAN("You are not in any teams!") );
+			return;
+		}
 		else
 		{
 			var olist = [];