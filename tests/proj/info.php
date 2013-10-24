e97961529fce1ee6a8ba356a15e5470c3a3c2aed
Peter Law <PeterJCLaw@gmail.com>
2013-10-24 22:58:39 +0100

    Move test logic into a function for re-use.
    Change-Id: I29ad81ee0f15a969a3d7d89b644b8044af4fd1da

--- tests/proj/info.php
+++ tests/proj/info.php
@@ -4,7 +4,6 @@ $repo_clone_url = '{ user: "%1$s", team: "%2$s", project: "%3$s" }';
 $user = 'bees';
 $team = 'ABC';
 $project = 'wasps';
-$expected_url = "{ user: \"$user\", team: \"$team\", project: \"$project\" }";
 
 $config = Configuration::getInstance();
 $config->override('repo_clone_url', $repo_clone_url);
@@ -21,16 +20,21 @@ test_true($auth->authUser($user,'face'), "authentication failed");
 // setup the required input keys
 $input = Input::getInstance();
 $input->setInput("team", $team);
-$input->setInput("project", $project);
 
-$mm = ModuleManager::getInstance();
-$mm->importModules();
-$proj = $mm->getModule('proj');
+function check_project($project, $expected_url) {
+    $input = Input::getInstance();
+    $input->setInput("project", $project);
 
-$proj->dispatchCommand('info');
+    $mm = ModuleManager::getInstance();
+    $mm->importModules();
+    $proj = $mm->getModule('proj');
+    $proj->dispatchCommand('info');
 
-$output = Output::getInstance();
-$repoUrl = $output->getOutput('repoUrl');
-test_nonnull($repoUrl, 'Failed to get repoUrl from output');
+    $output = Output::getInstance();
+    $repoUrl = $output->getOutput('repoUrl');
+    test_nonnull($repoUrl, 'Failed to get repoUrl from output');
 
-test_equal($repoUrl, $expected_url, "Wrong repo url data output");
+    test_equal($repoUrl, $expected_url, "Wrong repo url data output");
+}
+
+check_project('wasps', "{ user: \"$user\", team: \"$team\", project: \"wasps\" }");
