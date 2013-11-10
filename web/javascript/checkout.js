6705633a3aed8808241d5e5b6ee5f80a4cf745fe
Peter Law <PeterJCLaw@gmail.com>
2013-11-10 13:08:01 +0000

    Remove redundant checkout code.
    _getLocation appears to have been unused for quite a while, and
    nothing is using the export number any more (since the Java option
    has been removed for a while).
    
    Change-Id: Idf721f3e237cefb908e2538a9cc5a4d26b6efa80

--- web/javascript/checkout.js
+++ web/javascript/checkout.js
@@ -22,34 +22,18 @@ Checkout.prototype._basic = function(url, rev, successCallback, errorCallback) {
 	successCallback();
 }
 
-// Gets the current location without the hash or search parts
-Checkout.prototype._getLocation = function() {
-	return location.protocol + '//' + location.host + location.pathname;
-}
-
 Checkout.prototype._download = function(successCallback, errorCallback, nodes) {
 	var url = nodes.url;
 	var rev = IDE_hash_shrink(nodes.rev);
 	this._basic(url, rev, successCallback, errorCallback);
 }
 
-/* Initiates a checkout.
- * We also munge the successCallback to record the number of checkouts the user has done.
+/**
+ * Initiates a checkout.
  */
 Checkout.prototype.checkout = function(team, project, rev, successCallback, errorCallback) {
-	var setting = 'export.number';
-	var record_export = function() {
-		var exports = user.get_setting(setting);
-		if(exports == null) {
-			exports = 0;
-		}
-		var s = new Object();
-		s[setting] = exports+1;
-		user.set_settings(s);
-		successCallback();
-	}
 	// get URL
 	IDE_backend_request("proj/co", {team: team, project: project, rev: rev},
-	                    bind(this._download, this, record_export, errorCallback),
+	                    bind(this._download, this, successCallback, errorCallback),
 	                    errorCallback);
 }
