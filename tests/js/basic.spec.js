485716140da9b1ebd1e958e4c9adf367f8858e71
Peter Law <PeterJCLaw@gmail.com>
2013-12-21 12:45:22 +0000

    Add support for data-driven JS tests.
    This includes a somewhat hacky basic test to prove that it works.
    
    Change-Id: Ib5d1aca0c558569c2e0f44f68a076fb6c1308622

--- tests/js/basic.spec.js
+++ tests/js/basic.spec.js
@@ -1,7 +1,23 @@
 
+var jdp = require('./jasmine-data-provider/spec/SpecHelper.js');
+var using = jdp.using;
+
 describe("A suite that ought to pass", function() {
 	it("A single spec that checks things", function() {
 		expect(true).toBe(true);
 		expect(false).toBe(false);
+	});
+	it("should be able to see the jasmine-data-provider", function() {
+		expect(jdp.using).toBeDefined();
+	});
+	var lastNumber = 0;
+	using("some numbers", [1, 2, 3, 42], function(value) {
+		it("should not equal the last number", function() {
+			expect(value).not.toEqual(lastNumber);
+			lastNumber = value;
+		});
+	});
+	it("should have stored the last number", function() {
+		expect(lastNumber).toEqual(42);
 	});
 });
