20d71f42666aa1981d612e28421daa0972b06ba9
Peter Law <PeterJCLaw@gmail.com>
2014-02-08 20:14:27 +0000

    Fix #2304: Print the name of the project we just exported.
    This extends the existing printing of the revision exported to also
    include the name of the project, which is a very useful thing to be
    reminded of.
    
    Change-Id: Ie5dd6e5c9ee255cf161d0822bb993bb6459a0532

--- web/javascript/checkout.js
+++ web/javascript/checkout.js
@@ -13,19 +13,20 @@ Checkout.GetInstance = function() {
 	return Checkout.Instance;
 };
 
-Checkout.prototype._basic = function(url, rev, successCallback, errorCallback) {
+Checkout.prototype._basic = function(url, project, rev, successCallback, errorCallback) {
 	logDebug('Checking out code using basic file transfer');
 	getElement('robot-zip').src = url;
-	var revLink = A({href: url, target: '_blank', title: 'Download the zip again'}, rev);
+	var projRev = project + '@' + rev;
+	var revLink = A({href: url, target: '_blank', title: 'Download the zip again'}, projRev);
 	var span = SPAN(null, 'Exporting ', revLink, '.');
 	status_msg(span, LEVEL_INFO);
 	successCallback();
 };
 
-Checkout.prototype._download = function(successCallback, errorCallback, nodes) {
+Checkout.prototype._download = function(successCallback, errorCallback, project, nodes) {
 	var url = nodes.url;
 	var rev = IDE_hash_shrink(nodes.rev);
-	this._basic(url, rev, successCallback, errorCallback);
+	this._basic(url, project, rev, successCallback, errorCallback);
 };
 
 /**
@@ -34,6 +35,6 @@ Checkout.prototype._download = function(successCallback, errorCallback, nodes) {
 Checkout.prototype.checkout = function(team, project, rev, successCallback, errorCallback) {
 	// get URL
 	IDE_backend_request("proj/co", {team: team, project: project, rev: rev},
-	                    bind(this._download, this, successCallback, errorCallback),
+	                    bind(this._download, this, successCallback, errorCallback, project),
 	                    errorCallback);
 };
