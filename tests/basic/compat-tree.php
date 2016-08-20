818041c54fe71ad130eb36e8b0b572029ee08333
Peter Law <PeterJCLaw@gmail.com>
2016-08-20 18:46:21 +0100

    Extend tests for fileTreeCompat before changing it
    This adds testing of the initial tree as well as empty directories.
    Directories in general are moved to a similar mechanism as is used
    for their creation by the 'file/mkdir' endpoint.
    
    The initial (simpler) tree occurs both before any content is added
    as well as afterwards (the latter using a revision specifier to
    also validate the revision handling).
    
    Change-Id: If1984565282cd8ef5efd36387fc97b2d85bfc59d

--- tests/basic/compat-tree.php
+++ tests/basic/compat-tree.php
@@ -16,12 +16,49 @@ $projectManager = ProjectManager::getInstance();
 $projectManager->createRepository(1, 'cake');
 $repo = $projectManager->getUserRepository(1, 'cake', 'bees');
 
-$repo->gitMKDir('ninjas');
-$repo->putFile('ninjas/nuns', "the cake is a lie\n");
-$repo->gitMKDir('ninjas/z-ninjaChildren');
-$repo->putFile('ninjas/z-ninjaChildren/pirates', 'Captain Jack');
+function check_initial($tree)
+{
+    var_dump($tree);
+
+    test_equal($tree[0]['kind'], 'FILE', 'tested file had incorrect kind');
+    test_equal($tree[0]['name'], 'robot.py', 'tested file had incorrect name');
+    test_equal($tree[0]['path'], '/cake/robot.py', 'tested file had incorrect path');
+
+    test_false(isset($tree[0]['autosave']), 'Autosave should not be set (file)');
+
+    test_equal(count($tree), 1, 'Should only be a single item in the initial tree');
+}
+
+function mkdir_and_holder($repo, $path)
+{
+    // simulate 'file/mkdir'
+    $placeholder = $path.'/.directory';
+    $repo->gitMKDir($path);
+    $repo->createFile($placeholder);
+    return $placeholder;
+}
 
 $tree = $repo->fileTreeCompat('cake');
+check_initial($tree);
+
+$dir_holders[] = mkdir_and_holder($repo, 'ninjas');
+$repo->putFile('ninjas/nuns', "the cake is a lie\n");
+$dir_holders[] = mkdir_and_holder($repo, 'ninjas/z-ninjaChildren');
+$repo->putFile('ninjas/z-ninjaChildren/pirates', 'Captain Jack');
+$dir_holders[] = mkdir_and_holder($repo, 'spacey empty-dir');
+$dir_holders[] = mkdir_and_holder($repo, 'z-last');
+$repo->putFile('z-last/bacon', 'tasty');
+
+$repo->stage('ninjas/nuns');
+$repo->stage('ninjas/z-ninjaChildren/pirates');
+$repo->stage('z-last/bacon');
+array_map(array($repo, 'stage'), $dir_holders);
+$repo->commit('needed since we removed autosave', 'test-user', 'test@example.com');
+$repo->push();
+
+$tree = $repo->fileTreeCompat('cake');
+
+var_dump($tree);
 
 test_equal($tree[0]['kind'], 'FOLDER', 'tested folder had incorrect kind');
 test_equal($tree[0]['name'], 'ninjas', 'tested folder had incorrect name');
@@ -38,6 +75,22 @@ test_equal($tree[0]['children'][1]['children'][0]['path'], '/cake/ninjas/z-ninja
 test_equal($tree[1]['kind'], 'FILE', 'tested file had incorrect kind');
 test_equal($tree[1]['name'], 'robot.py', 'tested file had incorrect name');
 test_equal($tree[1]['path'], '/cake/robot.py', 'tested file had incorrect path');
+test_equal($tree[2]['kind'], 'FOLDER', 'tested folder had incorrect kind');
+test_equal($tree[2]['name'], 'spacey empty-dir', 'tested folder had incorrect name');
+test_equal($tree[2]['path'], '/cake/spacey empty-dir', 'tested folder had incorrect path');
+test_equal($tree[2]['children'], array(), 'empty folder should be empty');
+test_equal($tree[3]['kind'], 'FOLDER', 'tested folder had incorrect kind');
+test_equal($tree[3]['name'], 'z-last', 'tested folder had incorrect name');
+test_equal($tree[3]['path'], '/cake/z-last', 'tested folder had incorrect path');
+test_equal($tree[3]['children'][0]['kind'], 'FILE', 'tested sub-file had incorrect kind');
+test_equal($tree[3]['children'][0]['name'], 'bacon', 'tested sub-file had incorrect name');
+test_equal($tree[3]['children'][0]['path'], '/cake/z-last/bacon', 'tested sub-file had incorrect path');
+
 test_false(isset($tree[0]['autosave']), 'Autosave should not be set (folder)');
 test_false(isset($tree[0]['children'][0]['autosave']), 'Autosave should not be set (child file)');
 test_false(isset($tree[1]['autosave']), 'Autosave should not be set (root file)');
+
+test_equal(count($tree), 4, 'Should be four root items in the main test tree');
+
+$tree = $repo->fileTreeCompat('cake', '.', 'HEAD^');
+check_initial($tree);
