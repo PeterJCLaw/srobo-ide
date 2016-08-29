fbc5fd4b1fa63021506cce1037224e9713584f0f
Peter Law <PeterJCLaw@gmail.com>
2016-08-29 16:52:34 +0100

    Fix updateRepository testing
    It turns out that this wasn't actually testing anything as there
    were no upstream changes for it to validate. Add some so that it's
    useful and assert that the resulting changes are persisted.
    
    Change-Id: Iee69709bdf8d3392106e1deedd0732488ea6dd11

--- tests/basic/proj-manager.php
+++ tests/basic/proj-manager.php
@@ -35,8 +35,9 @@ $ret = $projectManager->copyRepository(1, $srcName, $projName);
 test_false($ret, 'did not block copying of a project with / in the name');
 test_false(is_dir($repopath), 'copied repo with / in the name!');
 
-section('updateRepository: autosave contents');
+section('Check updateRepository merges local changes with upstream');
 
+subsection('prepare repo');
 $projName = 'face';
 $repopath = $config->getConfig("repopath") . "/1/master/" . $projName . ".git";
 $projectManager->createRepository(1, $projName);
@@ -54,6 +55,18 @@ $repo->stage('committed-autosave');
 $repo->commit('commit message', 'jim', 'jim@someplace.com');
 $repo->push();
 
+subsection('prepare upstream commit');
+$otherRepo = $projectManager->getUserRepository(1, $projName, 'dave');
+test_nonnull($otherRepo, "Failed to get repo: 1/$projName/dave");
+
+$otherRepo->putFile('other-file', $otherContent = 'some other content');
+$otherRepo->stage('other-file');
+
+$otherRepo->commit('other message', 'Dave', 'dave@someplace.com');
+$otherRepo->push();
+unset($otherRepo);
+
+subsection('make local changes');
 // we've now got a repo with a couple of committed files.
 // so, we modify the state of the checkout, as autosaves do
 
@@ -64,13 +77,17 @@ $repo->putFile('autosave', $autosaveContent = 'some autosaved content');
 $folder = $repo->workingPath().'/some-folder';
 
 $autosavedFile = $repo->workingPath().'/autosave';
+$otherFile = $repo->workingPath().'/other-file';
 
-// update
+subsection('validate behaviour');
 $projectManager->updateRepository($repo, 'jim');
 
 // test the result
 test_existent($autosavedFile, 'Autosaved (uncommitted) files should remain after an update');
 test_existent($folder, 'Empty folders should remain after an update');
 
+test_existent($otherFile, 'File added upstream should now be present');
+
 test_equal($repo->getFile('autosave'), $autosaveContent, 'Content of autosaved (uncommitted) file should be preserved');
 test_equal($repo->getFile('committed-autosave'), $committedContent, 'Content of committed file plus an autosave should be preserved');
+test_equal($repo->getFile('other-file'), $otherContent, 'Content of upstream file');
